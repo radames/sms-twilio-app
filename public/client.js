aa4d83cde6227833ea76c827041d562a6becdac0
Glitch (what-do-you-think) <none>
2018-05-01 03:10:26 +0000

    📹👬 Checkpoint ./public/client.js:562109/175

--- public/client.js
+++ public/client.js
@@ -4,8 +4,9 @@
   fetch("https://what-do-you-think.glitch.me/getMessages")
     .then(d => d.json())
     .then(data => {
-       data.forEach( e=> {
-         e.media_content = media_content.json();
-       });
-  });
+       console.log(data);
+
+       data.forEach( e => (e.media_content = JSON.parse(e.media_content)))
+       console.log(data);
+    });
 })()
